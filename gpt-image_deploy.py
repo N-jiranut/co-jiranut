282019322c5cb6468b48e011b9052521a8f982f6
N-jiranut <jiranut.nat@gmail.com>
2025-06-29 22:44:47 +0700

    .

--- gpt-image_deploy.py
+++ gpt-image_deploy.py
@@ -12,13 +12,23 @@ while True:
     ret, image = cap.read()
 
     # img = cv2.resize(image, (128,128))
-
-    cv2.imshow("HI",image)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # img = img / 255
+    # img = numpy.expand_dims(img, axis=0)
+    
     
     if n-cn==20:
-        print("ok")
+        img = cv2.resize(image, (128,128))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = img / 255
+        img = numpy.expand_dims(img, axis=0)
+        prediction = model.predict(img)
+        predicted_index = numpy.argmax(prediction[0])
+        class_labels = ["five", 'two', 'zero']
+        print("Predicted class:", class_labels[predicted_index])
         cn=n
 
+    cv2.imshow("HI",image)
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
